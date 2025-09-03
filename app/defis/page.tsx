6ec22575f40350664d9ac77f8ba588eeb14ebaff
Boosti-officiel <contact.boosti@gmail.com>
2025-09-03 23:50:22 +0200

    Create page.tsx

---
+++ app/defis/page.tsx
@@ -0,0 +1,12 @@
+export default function Defis() {
+  return (
+    <main style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>Les défis</h1>
+      <ul>
+        <li>🔹 Boire 1L d’eau avant midi</li>
+        <li>🔹 10 min d’étirements</li>
+        <li>🔹 Ranger 1 zone en 15 min</li>
+      </ul>
+    </main>
+  );
+}
