a1efaff35dd1d53ff7d3a668a8d96b3b522cb75a
Boosti-officiel <contact.boosti@gmail.com>
2025-09-03 23:51:28 +0200

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <main style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>Espace Admin</h1>
+      <p>(on branchera l’auth ensuite)</p>
+    </main>
+  );
+}
